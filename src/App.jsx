201c9d166a7ae68a22622701400659048d67969b
Siva Narisetty <sivanarisetty@geotab.com>
2024-04-16 22:16:41 -0400

    Add main header component and modal

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import PostList from './components/PostList.jsx'
+import MainHeader from './components/MainHeader.jsx'
 
 function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <PostList />
+      <MainHeader/>
+      <main>
+        <PostList />
+      </main>
     </>
   )
 }
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,13 +1,37 @@
 import NewPost from './NewPost';
 import Post from './Post';
 import classes from './PostList.module.css';
+import { useState } from 'react';
+import Modal from './Modal';
 
 function PostList() {
+
+    const [enteredBody, setEnteredBody] = useState('');
+    const [enteredAuthor, setEnteredAuthor] = useState('');
+    const [showModal, setShowModal] = useState(true);
+
+    function changeBodyHandler(event) {
+        setEnteredBody(event.target.value);
+    }
+
+    function changeAuthorHandler(event) {
+        setEnteredAuthor(event.target.value);
+    }
+
+    function hideModalHandler(){
+        setShowModal(false);
+    }
+
     return (
         <>
-            <NewPost />
+            {
+                showModal && <Modal onClose={hideModalHandler}>
+                        <NewPost onBodyChange={changeBodyHandler} 
+                            onAuthorChange={changeAuthorHandler}/>
+                    </Modal>
+            }            
             <ul className={classes.posts}>
-                <Post author="Siva Karthik" description = "Developers are tired!"/>
+                <Post author={enteredAuthor} description = {enteredBody}/>
                 <Post author="Vaishnavi" description = "Nurses are kind"/>
                 <Post author="Chickpea" description = "Milk is tasty!"/>
             </ul>
