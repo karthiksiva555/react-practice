30ca8f28810e311c4e68257c25343c631b29b4cc
Siva Karthik Narisetty <karthik.siva555@gmail.com>
2024-04-20 16:57:19 -0400

    Loaders, actions and dynamic routes

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,40 +1,17 @@
 import Post from './Post';
 import classes from './PostList.module.css';
-import { useEffect, useState } from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 function PostList() {
 
-    const [posts, setPosts] = useState([]);
-
-    useEffect(()=>{
-        async function fetchPosts() {
-            const response = await fetch('http://localhost:8000/posts');
-            const resData = await response.json();
-            setPosts(resData.posts);
-        }
-        fetchPosts();
-    }, []) // This [] defines when to call the effect; 
-           // [] means no dependencies, so call the effect only on component load
-
-    // function addPostHandler(postData){
-    //     // setPosts([postData, ...posts]); Works but not recommended
-    //     setPosts((existingPosts) => [postData, ...existingPosts]);
-    //     // Send POST request to /posts api endpoint
-    //     fetch('http://localhost:8000/posts', {
-    //         method: 'POST',
-    //         body: JSON.stringify(postData),
-    //         headers: {
-    //             'Content-Type': 'application/json'
-    //         }
-    //     });
-    // }
+    const posts = useLoaderData();
 
     return (
         <>
             {
                 posts.length > 0 && 
                 <ul className={classes.posts}>
-                    {posts.map((post) => <Post key={post.body} author={post.author} body={post.body}/>)}
+                    {posts.map((post) => <Post key={post.id} author={post.author} body={post.body} id={post.id}/>)}
                 </ul>
             }
             {
--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,36 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Posts from './Posts.jsx'
-import './index.css'
-import NewPost from './components/NewPost.jsx';
-import { RouterProvider, createBrowserRouter} from 'react-router-dom';
-import RootLayout from './routes/RootLayout.jsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Posts, { loader as postsLoader } from "./Posts.jsx";
+import "./index.css";
+import NewPost, { action as newPostAction } from "./components/NewPost.jsx";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import RootLayout from "./routes/RootLayout.jsx";
+import PostDetails, { loader as postDetailsLoader } from "./components/PostDetails.jsx";
 
 const router = createBrowserRouter([
-  { path: '/', element: <RootLayout/>, children:[
-    { path: '/', element: <Posts/>, children: [
-      { path: '/create-post', element: <NewPost /> }
-    ]},
-    { path: '/inner', element: <h1>This element is under MainHeader</h1> }
-  ]},
-  { path: '/outer', element: <h1>This element is outside Layout</h1> }
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Posts />,
+        loader: postsLoader,
+        children: [
+          // Absolute path
+          { path: "/create-post", element: <NewPost />, action: newPostAction },
+          // Relative path
+          { path: ":postId", element: <PostDetails />, loader: postDetailsLoader }
+        ],
+      },
+      { path: "/inner", element: <h1>This element is under MainHeader</h1> },
+    ],
+  },
+  { path: "/outer", element: <h1>This element is outside Layout</h1> },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
